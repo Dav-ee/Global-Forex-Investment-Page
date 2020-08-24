eb7686d5d82f1e95c4edab64e389107f612d5c91
Dav_ee <50449267+Dav-ee@users.noreply.github.com>
2020-08-24 11:14:18 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,311 @@
+<?php
+// require https
+if ($_SERVER['HTTPS'] != "on") {
+  $url="https://".
+  $_SERVER['SERVER_NAME'].
+  $_SERVER['REQUEST_URL'];
+  header("location:$url");
+  exit;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	     <meta charset="utf-8">
+  <meta http-equiv="content language" content="en-us" />  
+    <title>CHEAP AND AFFORDABLE Forex Trading Signals</title>
+    <link rel="icon" type="image/png" href="assets/img/home.ico"/>
+
+
+          <!-- Bootstrap Core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom Fonts -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+  <link href="vendor/simple-line-icons/css/simple-line-icons.css" rel="stylesheet">
+  <!-- Custom CSS -->
+<!--   <link href="css/stylish-portfolio.min.css" rel="stylesheet"> -->
+  <link href="css/stylish-portfolio.css" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="us/Header-Blue.css">
+    <link rel="stylesheet" href="us/Footer-Basic.css">
+    <link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Overpass:300,400,400i,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="us/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="us/css/animate.css">
+    
+    <link rel="stylesheet" href="us/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="us/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="us/css/magnific-popup.css">
+
+    <link rel="stylesheet" href="us/css/aos.css">
+
+    <link rel="stylesheet" href="us/css/ionicons.min.css">
+
+    <link rel="stylesheet" href="us/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="us/css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="us/css/flaticon.css">
+    <link rel="stylesheet" href="us/css/icomoon.css">
+    <link rel="stylesheet" href="us/css/style.css">
+    <link rel="stylesheet" href="extra.css">
+
+</head>
+<body>
+
+
+ <!-- Navigation -->
+  <a class="menu-toggle rounded" href="#">
+    <i class="fas fa-bars"></i>
+  </a>
+  <nav id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+      <li class="sidebar-brand">
+        <a class="js-scroll-trigger" href="#">FOREX TRADING SIGNALS</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="js-scroll-trigger" href="#">Home</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="js-scroll-trigger" href="index.html">About</a>
+      </li>     
+   
+    </ul>
+  </nav>
+
+ <!-- Header -->
+  <header class="masthead d-flex">
+    <div class="container text-center my-auto">
+      <h2 class="mb-1 m-white">Cheap and Affordable forex trading signals</h2>
+      <h3 class="mb-5 m-white">
+        <em>The best forex signals provider since 2019 More than 35,000 pips provided</em>
+      </h3>
+      <a class="btn btn-primary btn-xl js-scroll-trigger" href="#">Find Out More</a>
+    </div>
+    <div class="overlay"></div>
+  </header>
+
+<div class="some-text">
+<div>	
+	<p class="small-txt">The world of investing can be a jungle. Bears. Danger at every turn. That’s why we at Forex Signals pride ourselves on being the best. Trained professionals to guide you through the financial wilderness.</p>
+</div>
+<div>
+	<h4 class="smaller-txt"><strong>We also offer special packages.</strong> The subscription investments you pay are secured. So, you should be happy signing up straight away to get additional benefits!</h4>
+</div>
+<div>
+	<p> <strong>Join us now</strong> and avail the best opportunities of earning major amounts of profits with the assistance of our Forex Signal services that alert you every now and then about up-to-date market situations.</p>
+</div>						
+</div>
+
+
+ <section class="ftco-counter ftco-intro" id="section-counter">
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="col-md-5 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 color-1 align-items-stretch">
+              <div class="text">
+                <span>We have served over:</span>
+                <strong class="number" id="num" data-number="35000">0</strong> Happy Clients
+              </div>
+            </div>
+          </div>
+          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 color-2 align-items-stretch">
+              <div class="text">
+              <span>We peak at :</span>
+               <strong class="number" id="num" data-number="300">0</strong>% Monthly ROl
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-counter ftco-intro" id="section-counter">
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="col-md-5 d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 color-1 align-items-stretch">
+              <div class="text">
+                <span>We serve over:</span>
+                <strong class="number" id="num" data-number="9000">0</strong> pips per month
+              </div>
+            </div>
+          </div>
+          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+            <div class="block-18 color-2 align-items-stretch">
+              <div class="text">
+                 <span>We have over:</span>
+               <strong class="number" id="num" data-number="164">0</strong>+ Million dollars under management
+              </div>
+            </div>
+          </div>
+ 
+        </div>
+      </div>
+    </section>
+
+<section> 
+<div class="mid-text">
+<p class="myline-1"></p> 
+  <!--   <li> <p>Are you planning to start forex?</p> Start with only $10 and get timely and analyzed signals</li> -->
+<p class="some-space">BUY ANY OF OUR  PACKAGES AND START MAKING MONEY VIA FOREX TRADING</p>
+
+<p class="myline-2"></p>
+</div>
+
+<div  class="mid-text">
+<p class="payme"> <strong style="color: #000; font-weight: 600; font-size: 20px;"> CURRENTLY ACCEPTED PAYMENT METHODS </strong>  </p>  
+<p style="color: blue">Bitcoin, Netteller, PayPal </p>
+<h5>If you can't find your preferred payment method please contact us.</h5>
+<img src="pay.png" alt="image here" class="image-responsiveness">
+<p class="note">Note**WE DO NOT COLLECT YOUR PAYMENT INFORMATION</p>
+</div>
+
+ </section>
+
+  <section class="ftco-section">
+      <div class="container" id="bring-it-top">
+        <div class="row">
+
+          <div class="col-md-4 d-flex ftco-animate">
+            <div class="blog-entry align-self-stretch">          
+              <div class="text p-4 d-block">
+                <h3 class="heading mb-4"><a href="#">BEGINNER SIGNALS </a></h3>
+                <div class="meta mb-3 cost">
+                  <h5> $10 </h5>
+                </div>              
+                <p>Get BEGINNER SIGNAL at  ONLY $10</p>             
+              </div>
+                <p class="bitcoin-text"> Purchase Via PayPal</p>
+<a href="#" class="block-20" style="background-image: url('pal.png');">   </a>
+
+  <p class="bitcoin-text-2">Order Via Bitcoin</p>
+                  <a href="#" class="block-20" style="background-image: url('bitcoin.png');">   </a>
+            </div>
+          </div>
+
+
+
+          <div class="col-md-4 d-flex ftco-animate">
+            <div class="blog-entry align-self-stretch">          
+              <div class="text p-4 d-block">
+                  <h3 class="heading mb-4"><a href="#">BRONZE SIGNALS</a></h3>        
+                  <div class="meta mb-3 cost">
+                  <h5>$20 </h5>
+                </div>
+              <p>With only $20, get Bronze Signals</p>
+              </div>
+                <p class="bitcoin-text"> Purchase Via PayPal</p>
+             <a href="#" class="block-20" style="background-image: url('pal.png');">   </a>
+             <p class="bitcoin-text-2">Order Via Bitcoin</p>
+                
+                  <a href="#" class="block-20" style="background-image: url('bitcoin.png');">   </a>
+            </div>
+
+          </div>
+
+
+
+
+          <div class="col-md-4 d-flex ftco-animate">
+            <div class="blog-entry align-self-stretch">         
+              <div class="text p-4 d-block">
+                 <h3 class="heading mb-4"><a href="#">SILVER SIGNALS</a></h3>
+                <div class="meta mb-3 cost">             
+                  <h5>$50 </h5>           
+                </div>
+ <p>Our Silver signals  cost from $50 USD</p>            
+              </div>
+                <p class="bitcoin-text"> Purchase Via PayPal</p>
+ <a href="#" class="block-20" style="background-image: url('pal.png');">   </a>
+  <p class="bitcoin-text-2">Order Via Bitcoin</p>
+      
+                  <a href="#" class="block-20" style="background-image: url('bitcoin.png');">   </a>
+            </div>
+          </div>
+
+
+
+            <div class="col-md-4 d-flex ftco-animate">
+            <div class="blog-entry align-self-stretch">         
+              <div class="text p-4 d-block">
+                 <h3 class="heading mb-4"><a href="#">GOLD SIGNALS</a></h3>
+                <div class="meta mb-3 cost">             
+                  <h5>$100 </h5>           
+                </div>
+ <p>Our Gold Signals  cost from $100 USD</p>            
+              </div>
+                <p class="bitcoin-text"> Purchase Via PayPal</p>
+ <a href="#" class="block-20" style="background-image: url('pal.png');">   </a>
+  <p class="bitcoin-text-2">Order Via Bitcoin</p>
+  
+                  <a href="#" class="block-20" style="background-image: url('bitcoin.png');">   </a>
+            </div>
+          </div>
+           
+      
+        </div>
+      </div>
+    </section>
+    
+
+     <footer class="ftco-footer ftco-section img">
+      <div class="container">
+       
+          <div class="col-md-12 text-center">
+
+            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script> | All rights reserved | Global Forex Investment </a>
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+
+
+
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+<!-- end of loader -->
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="js/main.js"></script>
+
+
+   <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/stylish-portfolio.min.js"></script>
+
+</body>
+</html>
